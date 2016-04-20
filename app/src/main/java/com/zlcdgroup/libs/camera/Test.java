f58f53a04ab2717fc18a1e50dcb7c8e8c701a50f
akingyin1987 <akingyin@163.com>
2016-04-20 15:16:53 +0800

    创建项目

---
+++ app/src/main/java/com/zlcdgroup/libs/camera/Test.java
@@ -0,0 +1,14 @@
+package com.zlcdgroup.libs.camera;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @ Description:
+ *
+ * Company:重庆中陆承大科技有限公司
+ * @ Author king
+ * @ Date 2016/4/20 15:16
+ * @ Version V1.0
+ */
+public class Test  extends AppCompatActivity{
+}
